f58d9c86f6432befd73de768caf7275439584abc
mohd adil <mohdadilrangrej@gmail.com>
2024-05-26 20:13:26 +0530

    subset done

--- backtracking/Subset.java
+++ backtracking/Subset.java
@@ -2,19 +2,20 @@
  * Subset
 */
 public class Subset {
-    public static void subset(String str ,String ans ,int i){
+    static int count = 1;
+    public static void subset(String str ,int i ,String ans){
         if (i==str.length()) {
-            if (ans == "") {
-                System.out.println("null");
-            }
-            System.err.println(ans);
+            if (ans == "")
+                System.out.println(STR."\{count}:null");
+            else
+                System.err.println(STR."\{count++}:\{ans}");
             return;
         }
-        subset(str, ans+str.charAt(i), i+1);
-        subset(str, ans, i+1);
+        subset(str, i+1, ans+str.charAt(i));
+        subset(str, i+1, ans);
     }
     public static void main(String[] args) {
         String str = "abc";
-       subset(str,"",0);
+       subset(str , 0 , "");
     }
 }
