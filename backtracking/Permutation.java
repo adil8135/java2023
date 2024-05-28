16ae343d7684d150343fdda9aab88d370c003e65
mohd adil <mohdadilrangrej@gmail.com>
2024-05-28 15:00:12 +0530

    solving another backtracking problem

---
+++ backtracking/Permutation.java
@@ -0,0 +1,21 @@
+public class Permutation {
+    
+    public static void permutation(String str , String ans){
+        if(str.length()==0){
+            System.out.print(STR."\{ans} ");
+            return;
+        }
+        for (int i = 0; i < str.length(); i++) {
+            char curr = str.charAt(i);
+            String newstr=str.substring(0,i)+str.substring(i+1);
+            permutation(newstr,ans+curr);
+        }
+
+    }
+    
+    public static void main(String[] args) {
+        String str = "abc";
+        permutation(str,"");
+
+    }
+}
